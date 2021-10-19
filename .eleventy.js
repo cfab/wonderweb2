3397d066c802223e885eafb02387bd1b08d8b659
Emmanuel Piguet <man@wonderweb.ch>
2021-10-19 10:05:24 +0200

    Add comments

--- .eleventy.js
+++ .eleventy.js
@@ -3,20 +3,26 @@ const now = String(Date.now())
 const { DateTime } = require("luxon");
 
 module.exports = function (eleventyConfig) {
+
+  // navigation plugin
   const pluginNavigation = require("@11ty/eleventy-navigation");
   eleventyConfig.addPlugin(pluginNavigation);
 
+  // tailwind css
   eleventyConfig.addWatchTarget('./styles/tailwind.config.js')
   eleventyConfig.addWatchTarget('./styles/tailwind.css')
 
+  // alpine js
   eleventyConfig.addPassthroughCopy({
     './node_modules/alpinejs/dist/cdn.js': './js/alpine.js',
   })
 
+  // files version.. useful ?
   eleventyConfig.addShortcode('version', function () {
     return now
   })
 
+  // minify html
   eleventyConfig.addTransform('htmlmin', function (content, outputPath) {
     if (
       process.env.ELEVENTY_PRODUCTION &&
@@ -34,12 +40,11 @@ module.exports = function (eleventyConfig) {
     return content
   })
 
-
-
-
+  // layouts
   eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
   eleventyConfig.addLayoutAlias("default", "layouts/default.njk");
 
+  // filtres pour single post
   eleventyConfig.addFilter('htmlDateString', (dateObj) => {
     return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
   });
